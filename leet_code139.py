5d081550536589f2cba88e5210314c1dae5a970f
Teja Mupparaju <teja.mupparaju@gmail.com>
2017-02-11 13:41:06 -0800

    Solution1

--- leet_code139.py
+++ leet_code139.py
@@ -15,4 +15,22 @@ Show Tags
 Show Similar Problems
 
 """
-.
+
+class Solution(object):
+    def wordBreak(self, s, wordDict):
+        """
+        :type s: str
+        :type wordDict: List[str]
+        :rtype: bool
+
+        Solution: two loops and compare
+        """
+        dp = [True]
+        dp += [False]*len(s)
+
+        for i in range(len(s)+1):
+            for j in range(i+1):
+                if dp[j] and s[j:i] in wordDict:
+                    dp[i] = True
+                    break
+        return dp[-1]
