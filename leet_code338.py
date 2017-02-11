d8261877bba9bfa2083f879aae02b14dfae5d6a1
Teja Mupparaju <teja.mupparaju@gmail.com>
2017-02-11 12:25:18 -0800

    Solution1

---
+++ leet_code338.py
@@ -0,0 +1,36 @@
+"""
+Given a non negative integer number num. For every numbers i in the range 0 ≤ i ≤ num calculate the number of 1's in their binary representation and return them as an array.
+
+Example:
+For num = 5 you should return [0,1,1,2,1,2].
+
+Follow up:
+
+It is very easy to come up with a solution with run time O(n*sizeof(integer)). But can you do it in linear time O(n) /possibly in a single pass?
+Space complexity should be O(n).
+Can you do it like a boss? Do it without using any builtin function like __builtin_popcount in c++ or in any other language.
+
+"""
+
+import math
+class Solution(object):
+    def countBits(self, num):
+        """
+        :type num: int
+        :rtype: List[int]
+        """
+        result = [0]
+        power_counter = 0
+        power_num = math.pow(2, power_counter)
+
+        for i in range(1, num+1):
+            if power_num == i:
+                result.append(1)
+                power_counter += 1
+                power_num = int(math.pow(2, power_counter))
+            else:
+                result.append(1+ result[i-power_num])
+
+        return result
+
+
