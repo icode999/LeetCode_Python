477e9bc10eb0f497a4c92b721f044e8dcf2dd871
Teja Mupparaju <teja.mupparaju@gmail.com>
2017-02-11 11:24:03 -0800

    Solution1

---
+++ leet_code322.py
@@ -0,0 +1,33 @@
+"""
+You are given coins of different denominations and a total amount of money amount.
+Write a function to compute the fewest number of coins that you need to make up that amount.
+If that amount of money cannot be made up by any combination of the coins, return -1.
+
+Example 1:
+coins = [1, 2, 5], amount = 11
+return 3 (11 = 5 + 5 + 1)
+
+Example 2:
+coins = [2], amount = 3
+return -1.
+
+Note:
+You may assume that you have an infinite number of each kind of coin.
+"""
+
+
+class Solution(object):
+    def coinChange(self, coins, amount):
+        """
+        :type coins: List[int]
+        :type amount: int
+        :rtype: int
+        """
+        dp = [0]
+        dp += [float('inf')]*amount
+
+        for i in range(1, amount+1):
+            dp[i] = min(1+dp[i-coin] if i-coin >= 0 else float('inf') for coin in coins)
+
+        return dp[amount] if dp[amount] != float('inf') else -1
+
