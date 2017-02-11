69b777da6fa0260e2dc60a64b0fa62241960e330
Teja Mupparaju <teja.mupparaju@gmail.com>
2017-02-11 15:09:56 -0800

    Solution1

--- leet_code152.py
+++ leet_code152.py
@@ -30,3 +30,29 @@ class Solution(object):
             result = max(result, temp1)
 
         return result
+
+
+# **  A little optimization to use o(1) space instead of o(n) space **
+class Solution(object):
+    def maxProduct(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+
+        Solution: for each point we save both postive and negative while maintaining current max (Came up)
+        """
+        max_here, min_here = [0,nums[0]] if nums[0]<=0 else [nums[0],0]
+        result = nums[0]
+
+        for i in nums[1:]:
+            if i <= 0:
+                t1 = min_here*i if min_here else 0
+                t2 = max_here*i if max_here else i
+            else:
+                t1 = max_here*i if max_here else i
+                t2 = min_here*i if min_here else 0
+
+            max_here, min_here = (t1, t2)
+            result = max(result, max_here)
+
+        return result
