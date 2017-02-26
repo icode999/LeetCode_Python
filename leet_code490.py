951c9227346cdb0e6f2026e7349952d73a4a25e1
Teja Mupparaju <teja.mupparaju@gmail.com>
2017-02-26 11:13:13 -0800

    Solution1

---
+++ leet_code490.py
@@ -0,0 +1,89 @@
+"""
+There is a ball in a maze with empty spaces and walls. The ball can go through empty spaces by rolling up, down, left or right, but it won't stop rolling until hitting a wall. When the ball stops, it could choose the next direction.
+
+Given the ball's start position, the destination and the maze, determine whether the ball could stop at the destination.
+
+The maze is represented by a binary 2D array. 1 means the wall and 0 means the empty space. You may assume that the borders of the maze are all walls. The start and destination coordinates are represented by row and column indexes.
+
+Example 1
+
+Input 1: a maze represented by a 2D array
+
+0 0 1 0 0
+0 0 0 0 0
+0 0 0 1 0
+1 1 0 1 1
+0 0 0 0 0
+
+Input 2: start coordinate (rowStart, colStart) = (0, 4)
+Input 3: destination coordinate (rowDest, colDest) = (4, 4)
+
+Output: true
+Explanation: One possible way is : left -> down -> left -> down -> right -> down -> right.
+
+Example 2
+
+Input 1: a maze represented by a 2D array
+
+0 0 1 0 0
+0 0 0 0 0
+0 0 0 1 0
+1 1 0 1 1
+0 0 0 0 0
+
+Input 2: start coordinate (rowStart, colStart) = (0, 4)
+Input 3: destination coordinate (rowDest, colDest) = (3, 2)
+
+Output: false
+Explanation: There is no way for the ball to stop at the destination.
+
+Note:
+There is only one ball and one destination in the maze.
+Both the ball and the destination exist on an empty space, and they will not be at the same position initially.
+The given maze does not contain border (like the red rectangle in the example pictures), but you could assume the border of the maze are all walls.
+The maze contains at least 2 empty spaces, and both the width and height of the maze won't exceed 100.
+Hide Company Tags Google
+Hide Tags
+
+"""
+#LUP
+class Solution(object):
+    def hasPath(self, maze, start, destination):
+        """
+        :type maze: List[List[int]]
+        :type start: List[int]
+        :type destination: List[int]
+        :rtype: bool
+
+        0 0 1 0 *
+        0 0 0 0 0
+        0 0 0 1 0
+        1 1 0 1 1
+        0 0 0 0 0
+        """
+        points = [start]
+        directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+        m, n = len(maze), len(maze[0])
+
+        while len(points) != 0:
+            x, y = points.pop(0)
+            maze[x][y] = "X"
+
+            if x == destination[0] and y == destination[1]:
+                return True
+
+            for dir in directions:
+                row = x + dir[0]
+                col = y + dir[1]
+
+                while 0 <= row < m and 0 <= col < n and maze[row][col] != 1:
+                    row += dir[0]
+                    col += dir[1]
+
+                row -= dir[0]
+                col -= dir[1]
+
+                if maze[row][col] == 0:
+                    points.append([row, col])
+
+        return False
