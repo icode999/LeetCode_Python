30d60194cd86c0cf6e9f2127da3753be6784c0c0
Teja Mupparaju <teja.mupparaju@gmail.com>
2017-03-21 14:24:03 -0700

    Solution1

--- leet_code240.py
+++ leet_code240.py
@@ -90,3 +90,24 @@ class Solution2(object):
                 if result:
                     return result
         return False
+
+# LUP idea
+#The idea is to search from the top-right element and then reduce the range for further searching by comparisons between target and the current element.
+
+# Suppose we want to search for 12. We first initialize r = 0 and c = 4. We compare 12 with matrix[r][c] = matrix[0][4] = 15 and 12 < 15, so 12 cannot appear in the column of 15 since all elements below 15 are not less than 15. Thus, we decrease c by 1 and reduce the search range by a column. Now we compare 12 with matrix[r][c] = matrix[0][3] = 11 and 12 > 11, so 12 cannot appear in the row of 11 since all elements left to 11 are not greater than 11. Thus, we increase r by 1 and reduce the search range by a row. Then we reach matrix[1][3] = 12 = target and we are done (return true). If we have moved beyond the matrix and have not found the target, return false.
+
+class Solution3(object):
+    def searchMatrix(self, matrix, target):
+        if not matrix:
+            return False
+
+        row, col = 0, len(matrix[0])-1
+        while row < len(matrix) and col >= 0:
+            if matrix[row][col] == target:
+                return True
+            elif matrix[row][col] < target:
+                row += 1
+            else:
+                col -= 1
+
+        return False
