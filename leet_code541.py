fa459d18fe1cbb0fac8fdc5536d44d40daf299c1
Teja Mupparaju <teja.mupparaju@gmail.com>
2017-03-20 21:37:27 -0700

    Solution1

---
+++ leet_code541.py
@@ -0,0 +1,44 @@
+"""
+541. Reverse String II
+
+Given a string and an integer k, you need to reverse the first k characters for every 2k characters counting from the start of the string. If there are less than k characters left, reverse all of them. If there are less than 2k but greater than or equal to k characters, then reverse the first k characters and left the other as original.
+Example:
+Input: s = "abcdefg", k = 2
+Output: "bacdfeg"
+Restrictions:
+The string consists of lower English letters only.
+Length of the given string and k will in the range [1, 10000]
+
+Hide Company Tags Google
+Show Tags
+Show Similar Problems
+"""
+
+# LUP solution
+class Solution(object):
+    def reverseStr(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: str
+        """
+        s = list(s)
+        for i in range(0, len(s), 2*k):
+            s[i:i+k] = reversed(s[i:i+k])
+        return ''.join(s)
+
+# MOWN solution
+class Solution(object):
+    def reverseStr(self, s, k):
+        s = list(s)
+        for i in range(0, len(s), 2*k):
+            s[i:i+k] = self.reverse(s[i: i+k])
+        return ''.join(s)
+
+    def reverse(self, s):
+        i, j = 0, len(s)-1
+        while i < j:
+            s[i], s[j] = s[j], s[i]
+            i += 1
+            j -= 1
+        return s
