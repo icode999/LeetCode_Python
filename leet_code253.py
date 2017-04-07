f35c5b7a9deb3dd9f65cf1d46b2b11ed42894bbd
Teja Mupparaju <teja.mupparaju@gmail.com>
2017-04-07 15:07:38 -0700

    Solution1

--- leet_code253.py
+++ leet_code253.py
@@ -30,7 +30,6 @@ class Solution(object):
         :rtype: int
         """
         pq = list()
-        count = 0
         rooms = 0
         for interval in sorted(intervals, key=lambda i: i.start):
             if pq and pq[0] <= interval.start:
@@ -38,5 +37,35 @@ class Solution(object):
             else:
                 heapq.heappush(pq, interval.end)
                 rooms += 1
+
+        return rooms
+
+# LUP solution
+# Very similar with what we do in real life. Whenever you want to start a meeting,
+# you go and check if any empty room available (available > 0) and
+# if so take one of them ( available -=1 ). Otherwise,
+# you need to find a new room someplace else ( numRooms += 1 ).
+# After you finish the meeting, the room becomes available again ( available += 1 ).
+
+class Solution(object):
+    def minMeetingRooms(self, intervals):
+        start, end = list(), list()
+        for interval in intervals:
+            start.append(interval.start)
+            end.append(interval.end)
+
+        start.sort()
+        end.sort(reverse=True)
+        rooms, available = 0, 0
+        for i in range(len(start)):
+            if end[-1] <= start[i]:
+                end.pop()
+                available += 1
+
+            if not available:
+                rooms += 1
+
+            else:
+                available -= 1
 
         return rooms
