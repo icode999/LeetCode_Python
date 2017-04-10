c781819c1e6d469495fb57e78ddaf8c8440ce5a1
Teja Mupparaju <teja.mupparaju@gmail.com>
2017-04-10 09:44:46 -0700

    Solution1

--- leet_code346.py
+++ leet_code346.py
@@ -41,3 +41,20 @@ class MovingAverage(object):
 # Your MovingAverage object will be instantiated and called as such:
 # obj = MovingAverage(size)
 # param_1 = obj.next(val)
+
+
+
+# LUP
+
+class MovingAverage(object):
+
+    def __init__(self, size):
+        self.vect, self.sums, self.idx, self.size = [0] * size, 0, 0, size
+
+
+    def next(self, val):
+        self.idx += 1
+        self.sums -= self.vect[self.idx % self.size]
+        self.vect[self.idx % self.size] = val
+        self.sums += val
+        return self.sums / float(min(self.idx, self.size))
