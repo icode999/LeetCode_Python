c71c6ccd296702ece8588f17ea0256f92431b28b
Teja Mupparaju <teja.mupparaju@gmail.com>
2017-04-20 18:38:24 -0700

    Solution1

---
+++ leet_code199.py
@@ -0,0 +1,53 @@
+"""
+199. Binary Tree Right Side View
+
+Given a binary tree, imagine yourself standing on the right side of it, return the values of the nodes you can see ordered from top to bottom.
+
+For example:
+Given the following binary tree,
+   1            <---
+ /   \
+2     3         <---
+ \     \
+  5     4       <---
+You should return [1, 3, 4].
+
+Credits:
+Special thanks to @amrsaqr for adding this problem and creating all test cases.
+
+Hide Company Tags Amazon
+Hide Tags Tree Depth-first Search Breadth-first Search
+Hide Similar Problems (M) Populating Next Right Pointers in Each Node (M) Boundary of Binary Tree
+"""
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+# Solution: BFS level order iterative
+class Solution(object):
+    def rightSideView(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        if not root:
+            return list()
+        result = list()
+        listr = [root]
+
+        while True:
+            tempr = list()
+            for node in listr:
+                for tnode in [node.left, node.right]:
+                    if tnode:
+                        tempr.append(tnode)
+            result.append(node.val)
+            if tempr:
+                listr = tempr[:]
+            else:
+                break
+
+        return result
