5dcb8d0943ee556d0b59346652f786c9625fdca0
tmupparaju <tejaswi.mupparaju@quanergy.com>
2017-11-10 16:12:39 -0800

    283

---
+++ leet_code383.py
@@ -0,0 +1,46 @@
+"""
+383. Ransom Note
+
+Given an arbitrary ransom note string and another string containing letters from all the magazines, write a function that will return true if the ransom note can be constructed from the magazines ; otherwise, it will return false.
+
+Each letter in the magazine string can only be used once in your ransom note.
+
+Note:
+You may assume that both strings contain only lowercase letters.
+
+canConstruct("a", "b") -> false
+canConstruct("aa", "ab") -> false
+canConstruct("aa", "aab") -> true
+
+"""
+
+#LUP
+from collections import Counter
+class Solution(object):
+    def canConstruct(self, ransomNote, magazine):
+        """
+        :type ransomNote: str
+        :type magazine: str
+        :rtype: bool
+        """
+        return not Counter(ransomNote)-Counter(magazine)
+
+
+# MOWN
+from collections import defaultdict
+class Solution(object):
+    def canConstruct(self, ransomNote, magazine):
+        """
+        :type ransomNote: str
+        :type magazine: str
+        :rtype: bool
+        """
+        mapr = defaultdict(int)
+        for char in magazine:
+            mapr[char] += 1
+
+        for char in ransomNote:
+            mapr[char] -= 1
+            if mapr[char] < 0:
+                return False
+        return True
