9e8a834abd0eec32b425717083d0c1e11bce32cd
tmupparaju <tejaswi.mupparaju@quanergy.com>
2017-11-19 14:58:48 -0800

    283

---
+++ leet_code515.py
@@ -0,0 +1,51 @@
+"""
+515. Find Largest Value in Each Tree Row
+
+You need to find the largest value in each row of a binary tree.
+
+Example:
+Input: 
+
+          1
+         / \
+        3   2
+       / \   \  
+      5   3   9 
+
+Output: [1, 3, 9]
+"""
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+# MOWN BFS
+from collections import deque
+class Solution(object):
+    def largestValues(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        if not root:
+            return list()
+
+        row = deque([root, 'end'])
+        result, cmax = list(), None
+
+        while row:
+            current_node = row.popleft()
+            if current_node == 'end':
+                if row:
+                    row.append('end')
+                result.append(cmax)
+                cmax = None
+                continue
+
+            cmax = max(cmax, current_node.val) if cmax != None else current_node.val
+            for node in [current_node.left, current_node.right]:
+                if node:
+                    row.append(node)
+        return result
