90c8c6da9d0146aa8863c0065ae4c48e0712e4d6
tmupparaju <tejaswi.mupparaju@quanergy.com>
2017-11-27 11:49:08 -0800

    283

--- leet_code364.py
+++ leet_code364.py
@@ -13,17 +13,12 @@ Given the list [[1,1],2,[1,1]], return 8. (four 1's at depth 1, one 2 at depth 2
 Example 2:
 Given the list [1,[4,[6]]], return 17. (one 1 at depth 3, one 4 at depth 2, and one 6 at depth 1; 1*3 + 4*2 + 6*1 = 17)
 
-Hide Company Tags LinkedIn
-Show Tags
-Show Similar Problems
-
 """
-
 # """
 # This is the interface that allows for creating nested lists.
 # You should not implement it, or speculate about its implementation
 # """
-#class NestedInteger(object):
+# class NestedInteger(object):
 #    def __init__(self, value=None):
 #        """
 #        If value is not specified, initializes an empty list.
@@ -37,7 +32,8 @@ Show Similar Problems
 #        """
 #
 #    def add(self, elem):
-#        """
+#        """Instead of multiplying by depth, add integers multiple times (by going level by level and adding the unweighted sum to the weighted sum after each level).
+
 #        Set this NestedInteger to hold a nested list and adds a nested integer elem to it.
 #        :rtype void
 #        """
@@ -62,63 +58,33 @@ Show Similar Problems
 #        :rtype List[NestedInteger]
 #        """
 
-# MOWN
-# solution save all element in a dict based on weight, at the end iterate throught that and multiply with adjusted weight
-from collections import defaultdict
+# LUP
+# Instead of multiplying by depth, add integers multiple times (by going level by level and adding the unweighted sum to the weighted sum after each level).
+
 class Solution(object):
     def depthSumInverse(self, nestedList):
         """
         :type nestedList: List[NestedInteger]
         :rtype: int
         """
-        mapr, maxer = defaultdict(list), 1
-        stack = [[1, iter(nestedList)]]
+        result = tresult = 0
 
-        while stack:
-            weight, iterator = stack.pop()
-            next_ele = next(iterator, None)
+        while nestedList:
+            next_level = list()
 
-            while next_ele:
-                if next_ele.isInteger():
-                    mapr[weight].append(next_ele.getInteger())
+            for nestInt in nestedList:
+                if nestInt.isInteger():
+                    tresult += nestInt.getInteger()
+
                 else:
-                    maxer = max(maxer, weight + 1)
-                    stack.append([weight+1, iter(next_ele.getList())])
+                    next_level += nestInt.getList()
 
-                next_ele = next(iterator, None)
-
-        adjust, result = maxer, 0
-        for i in range(1, maxer+1):
-            for val in mapr[i]:
-                result += val*adjust
-
-            adjust -= 1
+            nestedList = next_level[:]
+            result += tresult
 
         return result
 
-# LUP Solution
-# Instead of multiplying by depth, add integers multiple times (by going level by level and adding the unweighted sum to the weighted sum after each level).
 
-from collections import deque
-class Solution(object):
-    def depthSumInverse(self, nestedList):
-        que = deque([iter(nestedList)])
 
-        result, temp = 0, 0
-        while que:
-            iterator = que.popleft()
-            next_ele = next(iterator, None)
-            tque = list()
-            while next_ele:
-                if next_ele.isInteger():
-                    temp += next_ele.getInteger()
-                else:
-                    tque += next_ele.getList()
 
-                next_ele = next(iterator, None)
-            if tque:
-                que.append(iter(tque))
 
-            result += temp
-
-        return result
