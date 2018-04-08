17613b3538b9ac02eeddb1e4a9727aea4ab94345
tmupparaju <tejaswi.mupparaju@quanergy.com>
2018-04-07 19:29:17 -0700

    283

---
+++ leet_code377.py
@@ -0,0 +1,46 @@
+"""
+377. Combination Sum IV
+
+Given an integer array with all positive numbers and no duplicates, find the number of possible combinations that add up to a positive integer target.
+
+Example:
+
+nums = [1, 2, 3]
+target = 4
+
+The possible combination ways are:
+(1, 1, 1, 1)
+(1, 1, 2)
+(1, 2, 1)
+(1, 3)
+(2, 1, 1)
+(2, 2)
+(3, 1)
+
+Note that different sequences are counted as different combinations.
+
+Therefore the output is 7.
+Follow up:
+What if negative numbers are allowed in the given array?
+How does it change the problem?
+What limitation we need to add to the question to allow negative numbers?
+"""
+
+# LUP DP
+class Solution(object):
+    def combinationSum4(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        nums, dp = sorted(nums), [1] + [0]*target
+        for i in range(target+1):
+            for num in nums:
+                if i == num:
+                    dp[i] += 1
+                elif i > num:
+                    dp[i] += dp[i-num]
+                else:
+                    break
+        return dp[target]
