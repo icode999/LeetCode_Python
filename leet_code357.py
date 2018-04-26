841c01b93c9fe884b69a9261eed336121e230264
tmupparaju <tejaswi.mupparaju@quanergy.com>
2018-04-26 16:41:43 -0700

    283

--- leet_code357.py
+++ leet_code357.py
@@ -6,22 +6,42 @@ Given a non-negative integer n, count all numbers with unique digits, x, where 0
 Example:
 Given n = 2, return 91. (The answer should be the total numbers in the range of 0 <= x < 100, excluding [11,22,33,44,55,66,77,88,99])
 
+Companies
+Google
+
 '''
 
 # MOWN
+# Solution:
+#add a number and increment count, backtrack. Keep a map to have all used digits
 
-from collections import defaultdict
 class Solution(object):
-    def topKFrequent(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: List[int]
-        """
-        mapr = defaultdict(int)
-        for num in nums:
-            mapr[num] += 1
+    def __init__(self):
+        self.count = 1  # count 0
+        self.start = True
 
-        keys = mapr.keys()
-        keys.sort(key=lambda x: (-mapr[x], x))
-        return keys[:k]
+    def countNumbersWithUniqueDigits(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        self.Helper(n, {})
+        return self.count
+
+    def Helper(self, n, mapr):
+        if n == 0:
+            return
+
+        start = 1 if self.start else 0
+        self.start = False
+
+        for j in range(start, 10):
+            if mapr.has_key(j):
+                continue
+
+            self.count += 1
+            mapr[j] = True
+            self.Helper(n - 1, mapr)
+            mapr.pop(j)
+
+        return
