15d15180c347771224f6414efffb94c557e695d7
tmupparaju <tejaswi.mupparaju@quanergy.com>
2018-05-15 17:05:35 -0700

    283

---
+++ leet_code387.py
@@ -0,0 +1,30 @@
+"""
+387. First Unique Character in a String
+
+Given a string, find the first non-repeating character in it and return it's index. If it doesn't exist, return -1.
+
+Examples:
+
+s = "leetcode"
+return 0.
+
+s = "loveleetcode",
+return 2.
+Note: You may assume the string contain only lowercase letters.
+
+Companies
+Google Microsoft Amazon Bloomberg
+
+"""
+from collections import Counter
+class Solution(object):
+    def firstUniqChar(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        counter = Counter(s)
+        for idx, char in enumerate(s):
+            if counter[char] == 1:
+                return idx
+        return -1
