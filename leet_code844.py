368888a55d1894c2cbb8070fda65238f23e34289
tmupparaju <tejaswi.mupparaju@quanergy.com>
2018-06-06 21:13:03 -0700

    283

---
+++ leet_code844.py
@@ -0,0 +1,67 @@
+"""
+844. Backspace String Compare
+
+Given two strings S and T, return if they are equal when both are typed into empty text editors. # means a backspace character.
+
+Example 1:
+
+Input: S = "ab#c", T = "ad#c"
+Output: true
+Explanation: Both S and T become "ac".
+Example 2:
+
+Input: S = "ab##", T = "c#d#"
+Output: true
+Explanation: Both S and T become "".
+Example 3:
+
+Input: S = "a##c", T = "#a#c"
+Output: true
+Explanation: Both S and T become "c".
+Example 4:
+
+Input: S = "a#c", T = "b"
+Output: false
+Explanation: S becomes "c" while T becomes "b".
+
+
+Note:
+
+1 <= S.length <= 200
+1 <= T.length <= 200
+S and T only contain lowercase letters and '#' characters.
+
+Companies
+Google
+
+"""
+class Solution(object):
+    def backspaceCompare(self, S, T):
+        """
+        :type S: str
+        :type T: str
+        :rtype: bool
+        """
+
+        def nextr(string):
+            skip = 0
+            for char in reversed(string):
+                if char == "#":
+                    skip += 1
+
+                elif skip:
+                    skip -= 1
+
+                else:
+                    yield char
+
+        s_next, t_next = nextr(S), nextr(T)
+        while True:
+            x, y = next(s_next, None), next(t_next, None)
+            if x == y:
+                if x == None and y == None:
+                    break
+            else:
+                return False
+
+        return True
